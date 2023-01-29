6f5e6c4c52eeb4c7b73f50d0733a04a46aaf1b5f
Francisquet Vargas <fvs00725@gmail.com>
2023-01-28 23:30:14 -0600

    Added basic model for the database

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
+const Campground = require("./models/campground");
 
+mongoose.connect("mongodb://localhost:27017/yelp-camp");
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database Connected");
+});
 const app = express();
 
 const PORT = 3000;
