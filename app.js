b0bd409961e2d6698cdf678885b0de3168ba92e3
Francisquet Vargas <fvs00725@gmail.com>
2023-01-29 00:39:18 -0600

    Added a form and a POST route to add new campgrounds to our database

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ const PORT = 3000;
 app.set("view engine", "ejs")
 app.set("views", path.join(__dirname, "views"));
 
+app.use(express.urlencoded({extended: true}));
+
 app.get("/", (req, res) => {
     res.render("home");
 });
@@ -26,10 +28,21 @@ app.get("/campgrounds", async (req, res) => {
     res.render("campgrounds/index", { campgrounds });
 });
 
+app.get("/campgrounds/new", (req, res) => {
+    res.render("campgrounds/new");
+});
+
 app.get("/campgrounds/:id", async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.render("campgrounds/show", { campground });
 });
+
+app.post("/campgrounds", async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+});
+
 
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`);
