c3d24f701bb18f8a188e35e9b5e6b2d631e9f05f
Francisquet Vargas <fvs00725@gmail.com>
2023-02-15 17:15:28 -0600

    added functionality to fetch images and use them in our app

--- seeds/index.js
+++ seeds/index.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const Campground = require("../models/campground");
 const cities = require("./cities");
+const axios = require("axios");
 const {descriptors, places} = require("./seedHelpers");
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp");
@@ -17,13 +18,31 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            image: await seedImg(),
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title:  `${sample(descriptors)} ${sample(places)}`
+            title:  `${sample(descriptors)} ${sample(places)}`,
+            description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab adipisci, iste quaerat optio consectetur laborum delectus cum repellendus placeat veritatis quo dolore perspiciatis ullam nulla explicabo est ratione cupiditate vel.",
+            price
         });
         await camp.save();
     }
 };
+
+async function seedImg() {
+    try {
+      const resp = await axios.get('https://api.unsplash.com/photos/random', {
+        params: {
+          client_id: 'Rv-fY6bdOv_pZd-lavwDwJwzxNPPEQf9iiUTcITop7c',
+          collections: 1114848,
+        },
+      })
+      return resp.data.urls.small
+    } catch (err) {
+      console.error(err)
+    }
+}
 
 seedDB().then(() => {
     mongoose.connection.close();
