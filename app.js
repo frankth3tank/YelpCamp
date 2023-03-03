21e69bd9ace19d9dcf27f935ca9582680cb81152
Francisquet Vargas <fvs00725@gmail.com>
2023-03-03 09:58:39 -0600

    Added Joi for schema validation

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
+const Joi = require("joi");
+const { campgroundSchema } = require("./schemas.js")
 const catchAsync = require("./utils/catchAsync");
 const ExpressError = require("./utils/ExpressError");
 const Campground = require("./models/campground");
@@ -24,6 +26,17 @@ app.set("views", path.join(__dirname, "views"));
 app.engine("ejs", ejsMate);
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
+
+// Validate Schema middleware
+const validateCampground = (req, res, next) => {
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(",");
+        throw new ExpressError(msg, 400);
+    } else {
+        next();
+    }
+}
 
 app.get("/", (req, res) => {
     res.render("home");
@@ -48,14 +61,13 @@ app.get("/campgrounds/:id/edit", catchAsync(async (req, res) => {
     res.render("campgrounds/edit", { campground });
 }));
 
-app.post("/campgrounds", catchAsync(async (req, res, next) => {
-    if (!req.body.campground) throw new ExpressError("Invalid Campground Data", 400);
+app.post("/campgrounds", validateCampground, catchAsync(async (req, res, next) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
 }));
 
-app.put("/campgrounds/:id", catchAsync(async(req, res) => {
+app.put("/campgrounds/:id", validateCampground, catchAsync(async(req, res) => {
     const campground = await Campground.findByIdAndUpdate(req.params.id, {...req.body.campground});
     res.redirect(`/campgrounds/${campground._id}`);
 }));
